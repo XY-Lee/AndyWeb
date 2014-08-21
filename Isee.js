41bbb069bf79d22e95dfab4af109ff957134a706
a58116636 <a58116636@gmail.com>
2014-08-21 16:56:45 +0800

    加了jquery
    加了jquery

---
+++ Isee.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+  $("#video-1").click( function() {
+    $("#video-1").html("<br><iframe width=\"540\" height=\"360\" src=\"//www.youtube.com/embed/6Fael8CzfxU\" frameborder=\"0\" allowfullscreen></iframe>");
+  }) ;
+  
+  $("#video-2").click( function() {
+    $("#video-2").html("<br><iframe width=\"540\" height=\"360\" src=\"//www.youtube.com/embed/8NTx8UJVX3A\" frameborder=\"0\" allowfullscreen></iframe>");
+  }) ;
+  
+}) ;
